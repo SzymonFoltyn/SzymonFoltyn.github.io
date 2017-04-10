658b3fadbc01c570e895990619fe5a2403bdd63c
SzymonFoltyn <szymon.foltyn@gmail.com>
2017-04-10 20:35:30 +0200

    v.3 (Add hover to ikon and links to contact

--- js/script.js
+++ js/script.js
@@ -1,10 +1,36 @@
-$(document).on('click', 'a', function (event) {
+$(document).on('click', '#nav li a', function (event) {
     event.preventDefault();
 
     $('html, body').animate({
         scrollTop: $($.attr(this, 'href')).offset().top
     }, 500);
-    
+
     $('#nav li a').removeClass('active');
-        $(this).addClass('active');
+    $(this).addClass('active');
+});
+
+$(function () {
+    $('.ikon').hover(function () {
+        $('.hover').show();
+    }, function () {
+        $('.hover').hide();
+    });
+});
+
+$(function() {
+    var moveLeft = 20;
+    var moveDown = 10;
+
+    $('.ikon').hover(function(e) {
+        $('.hover').show()
+            .css('top', e.pageY + moveDown)
+            .css('left', e.pageX + moveLeft)
+            .appendTo('body');
+    }, function() {
+        $('.hover').hide();
+    });
+
+    $('.ikon').mousemove(function(e) {
+        $(".hover").css('top', e.pageY + moveDown).css('left', e.pageX + moveLeft);
+    });
 });
